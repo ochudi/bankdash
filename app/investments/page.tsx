d0b44948e08a54ad5de3502d4bca1712e5eb9b56
ochudi <chukwudi.ofoma@pau.edu.ng>
2025-04-03 12:38:13 +0100

    Implement investment page

--- app/investments/page.tsx
+++ app/investments/page.tsx
@@ -44,8 +44,8 @@ export default function InvestmentsPage() {
           <MonthlyRevenue />
         </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-          <div className="col-span-2">
+        <div className="grid grid-cols-1 xl:grid-cols-3 gap-6">
+          <div className="col-span-1 xl:col-span-2">
             <MyInvestment />
           </div>
           <div className="col-span-1">
